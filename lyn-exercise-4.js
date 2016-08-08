d300dd8d603a122136adafaa159d32922b9c4998
Mike Skalandunas <mskalandunas@gmail.com>
2016-08-08 10:40:19 -0700

    exercise 4

---
+++ lyn-exercise-4.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+function callback(err, data) {
+  if (err) {
+    throw err;
+  };
+
+  console.log(data.split('\n').length - 1);
+};
+
+fs.readFile(process.argv[2], 'utf8', callback);
