fd75dd45b77150508d088b1b799f9ac2a1b5488d
Mike Skalandunas <mskalandunas@gmail.com>
2016-08-12 18:17:37 -0700

    add changes

--- lyn-exercise-6-b.js
+++ lyn-exercise-6-b.js
@@ -5,6 +5,8 @@ const path = require('path')
 
 module.exports = function(dir, ext, callback) {
   fs.readdir(dir, (err, data) => {
+    if (err) throw err
+
     for (let i in data) {
       if (path.extname(data[i]) == '.' + ext) {
         console.log(data[i])
