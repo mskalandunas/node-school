67f0706f92a1298abf1b1e84e5fce7370a061330
Mike Skalandunas <mskalandunas@gmail.com>
2016-08-12 21:58:10 -0700

    add exercise 7

---
+++ learnyounode/lyn-exercise-7.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const http = require('http')
+let url = process.argv[2]
+
+http.get(url, (res) => {
+  res.setEncoding('utf8')
+  res.on('data', (data) => {
+    console.log(data)
+  })
+}).on('error', console.error)
