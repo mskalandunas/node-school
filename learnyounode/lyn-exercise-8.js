04de0bc1c1dbc4e0a6c63d9ebacb690332b5018d
Mike Skalandunas <mskalandunas@gmail.com>
2016-08-12 22:46:46 -0700

    add exercise 8 changes

--- learnyounode/lyn-exercise-8.js
+++ learnyounode/lyn-exercise-8.js
@@ -2,8 +2,8 @@ const http = require('http')
 const bl = require('bl')
 let url = process.argv[2]
 
-http.get(url, (response) => {
-  response.pipe(bl((err, data) => {
+http.get(url, (res) => {
+  res.pipe(bl((err, data) => {
     if (err)
       return console.error(err)
     data = data.toString()
